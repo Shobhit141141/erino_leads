dab47845b9c55f2a00a6e2d84c5fa2c8d1b2e475
Shobhit Tiwari <122107079+Shobhit141141@users.noreply.github.com>
2025-08-25 11:04:54 +0530

    feat: dynamic log directory in server

--- backend/src/utils/logger.js
+++ backend/src/utils/logger.js
@@ -1,4 +1,11 @@
+const path = require("path");
+const fs = require("fs");
 const winston = require("winston");
+
+const logDir = path.join(__dirname, "..", "logs");
+if (!fs.existsSync(logDir)) {
+  fs.mkdirSync(logDir);
+}
 
 const logLevels = {
   error: 0,
@@ -17,7 +24,6 @@ winston.addColors(logColors);
 
 const logFormat = winston.format.combine(
   winston.format.timestamp({ format: "YYYY-MM-DD HH:mm:ss" }),
-  winston.format.colorize({ all: true }),
   winston.format.printf(
     (info) => `[${info.timestamp}] ${info.level}: ${info.message}`
   )
@@ -25,11 +31,24 @@ const logFormat = winston.format.combine(
 
 const logger = winston.createLogger({
   levels: logLevels,
-
   format: logFormat,
+  transports: [
+    new winston.transports.Console({
+      format: winston.format.combine(
+        winston.format.colorize({ all: true }),
+        logFormat
+      ),
+    }),
 
-  transports: [new winston.transports.Console()],
+    new winston.transports.File({
+      filename: path.join(logDir, "error.log"),
+      level: "error",
+    }),
 
+    new winston.transports.File({
+      filename: path.join(logDir, "combined.log"),
+    }),
+  ],
   exitOnError: false,
 });
 
